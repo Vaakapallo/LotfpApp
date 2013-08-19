3cf6df57e25d1f75af54171d833bd890fb9d4397
Lassi Vapaakallio <Lassi.Vapaakallio@cs.helsinki.fi>
2013-08-19 20:06:07 +0300

    Equipment

--- LotfpApp/src/lotfpapp/Equipment.java
+++ LotfpApp/src/lotfpapp/Equipment.java
@@ -4,10 +4,36 @@
  */
 package lotfpapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Lassi
  */
 public class Equipment {
+
+    private List<Item> items;
+
+    public Equipment() {
+        items = new ArrayList();
+    }
+    
+    public void addItem(Item item){
+        items.add(item);
+    }
+    
+    public void addItem(String name){
+        this.addItem(new Item(name));
+    }
+    
+    public void addItem(String name, int price){
+        this.addItem(new Item(name, price));
+    }
+    
+    public void addEncumberingItem(String name){
+        this.addItem(new Item(name, true));
+    }
+    
     
 }
---
+++ LotfpApp/src/lotfpapp/Item.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lotfpapp;
+
+import java.util.List;
+
+/**
+ *
+ * @author Lassi
+ */
+public class Item {
+
+    private String name;
+    private int price;
+    private boolean encumbering = false;
+    private List<String> notes;
+
+    public Item(String name) {
+        this.name = name;
+    }
+    
+    public Item(String name, boolean encumbering){
+        this(name);
+        this.encumbering = encumbering;
+    }
+
+    public Item(String name, int price) {
+        this(name);
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void addNote(String note) {
+        notes.add(note);
+    }
+}
